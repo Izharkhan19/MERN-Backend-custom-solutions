dfc9ffe16475bd76cef9bfc96e062abc2b22de41
Izharkhan2022 <izhar.khan@softqubes.com>
2024-02-20 12:08:02 +0530

    change cluster

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb+srv://bt23052001:vNI8kXQjM3jeLAS9@cluster0.hfby3.mongodb.net/react+node?retryWrites=true&w=majority",
+  // url: "mongodb+srv://bt23052001:vNI8kXQjM3jeLAS9@cluster0.hfby3.mongodb.net/react+node?retryWrites=true&w=majority",
+  url: "mongodb+srv://Izhar01:Izhar01mongodb@cluster0.lribs3p.mongodb.net/Custom_Component_DB?retryWrites=true&w=majority",
 };
